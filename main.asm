557bb3d052fde172ace965d958a6cf29d38f7315
Pasakorn Cheeraachanon <pchee6093@gmail.com>
2026-09-09 21:30:11 +0700

    data segment

--- main.asm
+++ main.asm
@@ -1,1 +1,12 @@
 ; test git clone and push
+.data
+    ; permuted choice 1
+    PC_1 byte
+
+    ; permuted choice 2
+    PC_2 byte
+
+    ; for shift
+    SHIFT byte 1,1,2,2,2,2,2,2,1,2,2,2,2,2,2,,1
+
+.code
