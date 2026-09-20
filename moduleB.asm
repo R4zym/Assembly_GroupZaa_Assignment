f143f175f36eecc89fafc1a2f1387eeee0e82119
Pasakorn Cheeraachanon <pchee6093@gmail.com>
2026-09-20 14:29:25 +0700

    lol

--- moduleB.asm
+++ moduleB.asm
@@ -1,3 +1,8 @@
+TITLE Computer Organization and Assembly Language - Group Assignment 2026
+INCLUDE Irvine32.inc
+
+.386
+.model flat, stdcall
 .data
     ; permuted choice 1
          ; C0 first 28 bits
@@ -20,11 +25,211 @@
          byte   44,49,39,56,34,53
          byte   46,42,50,36,29,32
     ; for shift
-    SHIFT byte  1,1,2,2,2,2,2,2,1,2,2,2,2,2,2,,1
+    SHIFT byte  1,1,2,2,2,2,2,2,1,2,2,2,2,2,2,1
     ; for input KEYS (64 bits)
-     
+    KEYS_INPUT dword 32 DUP(0)
+    ; for key64 after {doing something}
+    KEYS_64 byte 8 DUP(0)
+    ; for user input (temp only)
+    USER_INPUT byte "Enter 64-bits DES Key:" ", 0
+    MSG_OK byte "Key accepted.", 0Dh, 0Ah, 0
+    MSG_ERROR byte "Invalid, Try again.", 0Dh, 0Ah, 0
 
 
+    ; ---------------------------------------------------------------
+    ; type something
+    ; ---------------------------------------------------------------
 
 
 .code
+
+main PROC
+    ; ---------------------------------------------------------------
+    ; รับ input  key
+    ; ---------------------------------------------------------------
+     mov edx, OFFSET USER_INPUT
+     call WriteString
+
+     mov edx, OFFSET KEYS_INPUT
+     mov ecx, SIZEOF KEYS_INPUT - 1
+     call ReadString
+
+     ; check 16 
+     cmp eax, 16
+     jne WrongKey
+
+     ; hex to 8 byte
+     mov esi, OFFSET KEYS_INPUT
+     mov edi, OFFSET KEY_64
+
+     call HexToByte
+
+     cmp eax, 0
+     je WrongKey
+
+     mov edx, OFFSET MSG_OK
+     call WriteString
+
+     exit
+
+WrongKey:
+     mov edx, OFFSET KMSG_ERROR
+     call WriteString
+
+     exit
+
+main ENDP
+
+    ; ---------------------------------------------------------------
+    ; description = change 12345678h to 12 34 56 78 
+    ; ---------------------------------------------------------------
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+HexToByte PROC
+     
+     push ebp 
+     mov ebp, esp
+
+     push ebx
+     push ecx
+     push edx
+     push esi
+     push edi
+
+     xor ecx, ecx 
+
+ConvertLoop: 
+
+     cmp ecx, 8
+     jge ConvertDone
+
+     ; first digit
+     mov al, [esi]
+     call HexToValue
+
+     cmp eax, -1
+     je ConvertError
+
+     mov ebx, eax
+
+     shl ebx, 4
+
+     ; second digit
+     mov al, [esi + 1]
+     call HexToValue
+
+     cmp eax, -1
+     je ConvertError
+
+     or ebx, eax
+
+     ; keep byte
+     mov [edi], bl 
+     
+     inc edi
+     add esi, 2
+
+     inc ecx
+     jmp ConvertLoop
+
+ConvertDone: 
+     mov eax, 1
+     jmp ConvertExit
+
+ConvertError:
+     xor eax, eax
+
+ConvertExit: 
+     pop edi
+     pop esi
+     pop edx
+     pop ecx
+     pop ebx
+
+     pop ebp
+
+     ret
+
+HexToByte ENDP
+
+
+     
+
